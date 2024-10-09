2b5da4c5256d3069d2f17d14c8ecf8f3bd7da481
Arifuzzaman Akash <mdarifuzzaman982k@gmail.com>
2024-10-10 04:49:36 +0600

    Custom form for searchProduct

--- todo_app/views.py
+++ todo_app/views.py
@@ -4,13 +4,32 @@ from .models import Products
 # Create your views here.
 def load_product(request):
     products = Products.objects.all()
+    
+    search_term = request.GET.get("queary")
+    search_product = []
+    
+    
     completed = request.GET.get("completed")
     if completed == '1':
         products = products.filter(completed = True)
     elif completed == '0':
         products = products.filter(completed = False)
         
-    return render(request, "loadproducts.html", {"products": products})
+    for product in products:
+        if search_term and search_term.lower() in product.title.lower():
+            search_product.append(product)
+            
+    if search_term:
+        context = {
+            "products": search_product
+        }
+    else:
+         context = {
+            "products": products
+        }
+        
+        
+    return render(request, "loadproducts.html", context=context)
 
 def product_details(request, pk):
     product = Products.objects.get(pk=pk)
